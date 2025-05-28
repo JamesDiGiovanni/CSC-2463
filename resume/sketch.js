15ee7526d2a8bc0f46229cc82f7e137c5ca3e487
JamesDiGiovanni <143350879+JamesDiGiovanni@users.noreply.github.com>
2025-05-28 13:17:39 -0500

    Add files via upload

---
+++ resume/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, 150);
+  background('#f0f0f0');
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  fill(50);
+  // text("Welcome to My Resume Page", width / 2, height / 2);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, 150);
+}
